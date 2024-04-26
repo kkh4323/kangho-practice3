991f80d675ffbf6df2fd454798505090f2731871
Kangho <kkh4323@naver.com>
2024-04-27 07:37:51 +0900

    entity 설정, CRUD

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { CreateBlogDto } from './dto/create-blog.dto';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+  constructor(private readonly blogService: BlogService) {}
+
+  // 블로그 전체 데이터 가져오는 api
+  @Get()
+  async getBlogs() {
+    return await this.blogService.getBlogDatas();
+  }
+
+  // 상세 블로그 데이터 가져오는 api
+  @Get('/:id')
+  async getBlogById(@Param('id') id: string) {
+    return await this.blogService.getBlogById(id);
+  }
+
+  // 블로그 등록 api
+  @Post('/create')
+  async createBlog(@Body() createBlogDto: CreateBlogDto) {
+    return await this.blogService.createBlog(createBlogDto);
+  }
+
+  // 블로그 수정 api
+  @Put('/:id')
+  async updateBlogById(
+    @Param('id') id: string,
+    @Body() updateBlogDto: CreateBlogDto,
+  ) {
+    return await this.blogService.updateBlogById(id, updateBlogDto);
+  }
+
+  // 블로그 삭제 api
+  @Delete('/:id')
+  async deleteBlogById(@Param('id') id: string) {
+    return await this.blogService.deleteBlogById(id);
+  }
+}
